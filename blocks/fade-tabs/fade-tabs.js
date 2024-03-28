1d4f902ffd319dd4bee062d0f3828d60c0ff9814
Tracy Tucker <tracy.tucker@slalom.com>
2024-03-28 07:15:39 -0500

    Adds mobile logic and styling for fade-tabs

--- blocks/fade-tabs/fade-tabs.js
+++ blocks/fade-tabs/fade-tabs.js
@@ -5,6 +5,72 @@ function hasWrapper(el) {
   return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
 }
 
+// Create <p> class names to distinguish styling
+function createClassNames(block) {
+  if (block.classList.contains('mobile')) {
+    const panels = block.querySelectorAll('.tabs-panel');
+    panels.forEach((panel) => {
+      for (let i = 0; i < panel.children[0].children.length; i += 1) {
+        panel.children[0].children[i].classList.add(`row-${i}`);
+      }
+    });
+  }
+}
+
+function createMobileTabNavigation() {
+  const tabsContainer = document.querySelector('.mobile-offer-tabs');
+  if (tabsContainer) {
+    const tablist = tabsContainer.querySelector('.tabs-list');
+    const leftTab = tabsContainer.querySelector('.arrow-left');
+    const rightTab = tabsContainer.querySelector('.arrow-right');
+    const { clientWidth, scrollWidth } = tablist;
+    tablist.addEventListener('scroll', (e) => {
+      const maxScrollWidth = scrollWidth - (clientWidth + e.target.scrollLeft);
+      leftTab.classList.remove('hide');
+      leftTab.classList.toggle('hide', e.target.scrollLeft <= 0);
+      rightTab.classList.toggle('hide', scrollWidth - (clientWidth + e.target.scrollLeft) <= maxScrollWidth + 1);
+    });
+
+    leftTab.addEventListener('click', (e) => {
+      e.preventDefault();
+      tablist.scrollLeft -= 150;
+    });
+
+    rightTab.addEventListener('click', (e) => {
+      e.preventDefault();
+      tablist.scrollLeft += 150;
+    });
+  }
+}
+
+// Creates a wrapper to group together tab list and navigation
+async function createMobileWrapper(block, tablist) {
+  if (block.classList.contains('mobile')) {
+    const buttonWrapper = block.closest('.mobile-offer-tabs').querySelector('.default-content-wrapper');
+    const tabListWrapper = document.createElement('div');
+    const tabIndicator = document.createElement('div');
+    const tabSliderWrapper = document.createElement('div');
+    tabListWrapper.className = 'tabs-list-wrapper';
+    tabIndicator.className = 'tabs-indicator';
+    tabSliderWrapper.className = 'tabs-slider-wrapper';
+
+    // Remove default <p> tag from navigation buttons
+    const pTag = buttonWrapper.children[0];
+    const aTags = Array.from(pTag.children);
+    aTags[0].className = 'tab-control arrow-left hide';
+    aTags[1].className = 'tab-control arrow-right';
+    aTags.forEach((item) => {
+      tabSliderWrapper.appendChild(item);
+    });
+    pTag.remove();
+    buttonWrapper.remove();
+    tabListWrapper.appendChild(tablist);
+    tabListWrapper.insertBefore(tabIndicator, tabListWrapper.children[0]);
+    tabSliderWrapper.appendChild(tabListWrapper);
+    block.insertBefore(tabSliderWrapper, block.children[0]);
+  }
+}
+
 // Builds tabList
 export default async function decorate(block) {
   // build tablist
@@ -65,4 +131,8 @@ export default async function decorate(block) {
   });
 
   block.prepend(tablist);
+
+  createMobileWrapper(block, tablist);
+  createClassNames(block);
+  createMobileTabNavigation();
 }
