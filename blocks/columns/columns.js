691d05096f02ff3664feedecbf2aa490dd6d696e
Tracy Tucker <tracy.tucker@slalom.com>
2024-04-04 12:11:14 -0500

    Adds dropdown animation for response screen size

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,3 +1,26 @@
+// Animates the dropdown items for footer-middle-top-columns in mobile view
+function animateDropdown(col) {
+  const minus = col.querySelectorAll('.icon-plus');
+  minus.forEach((item) => item.parentNode.classList.add('active'));
+  const titles = col.querySelectorAll('.title');
+  titles.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      if (e.currentTarget === item) {
+        e.preventDefault();
+        item.classList.toggle('active');
+        const icons = item.querySelectorAll('.icon');
+        icons.forEach((icon) => icon.parentNode.classList.toggle('active'));
+        const content = item.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = `${content.scrollHeight}px`;
+        }
+      }
+    });
+  });
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -42,8 +65,31 @@ export default function decorate(block) {
         });
       }
 
-      // footer bottom columns
+      // footer columns
+      if (col.closest('.footer-top-columns') !== null) {
+        col.parentNode.classList.add('footer-column-wrapper');
+        col.classList.add('footer-column');
+        col.classList.add(`footer-column-${i + 1}`);
+      }
+      if (col.closest('.footer-middle-top-columns') !== null) {
+        const titleDiv = document.createElement('div');
+        titleDiv.classList.add('title');
+        col.parentNode.classList.add('footer-column-wrapper');
+        col.classList.add('footer-column');
+        col.classList.add(`footer-column-${i + 1}`);
+        const colArr = Array.from(col.children);
+        const titleEl = colArr.filter((item) => item.tagName !== 'UL');
+        titleEl.forEach((item) => titleDiv.appendChild(item));
+        col.insertBefore(titleDiv, col.children[0]);
+        animateDropdown(col);
+      }
+      if (col.closest('.footer-middle-bottom-columns') !== null) {
+        col.parentNode.classList.add('footer-column-wrapper');
+        col.classList.add('footer-column');
+        col.classList.add(`footer-column-${i + 1}`);
+      }
       if (col.closest('.footer-bottom-columns') !== null) {
+        col.parentNode.classList.add('footer-column-wrapper');
         col.classList.add('footer-column');
         col.classList.add(`footer-column-${i + 1}`);
       }
